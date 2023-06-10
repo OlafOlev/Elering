9e521fdfcd1f08a98e420b619291c798cac6e8ca
OlafOlev <oalfolev@gmail.com>
2023-06-10 18:47:37 +0300

    Elering

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import app from './App';
 import reportWebVitals from './reportWebVitals';
+import cors from "cors";
 
+app.use(cors())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <app/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
